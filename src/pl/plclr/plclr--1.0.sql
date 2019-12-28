c04e5a9b8109cee9bbb4c36968efaa3149573032
Brar Piening <brar@gmx.de>
2019-12-28 17:20:07 +0100

    Initial testing for Pl/CLR
    Tests still fail because of the lacking implementation but
    the infrastructure works.

---
+++ src/pl/plclr/plclr--1.0.sql
@@ -0,0 +1,5 @@
+/* src/pl/plclr/plclr--1.0.sql */
+
+CREATE LANGUAGE plclr;
+
+COMMENT ON LANGUAGE plclr IS 'PL/CLR procedural language';
---
+++ src/pl/plclr/plclr_runtime_host.h
@@ -0,0 +1,8 @@
+#ifndef PLCLR_RUNTIME_HOST_H
+#define PLCLR_RUNTIME_HOST_H
+
+#include "plclr.h"
+
+void plclr_runtime_host_init(void);
+
+#endif // PLCLR_RUNTIME_HOST_H
