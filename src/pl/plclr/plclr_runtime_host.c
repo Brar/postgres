6f3e6819ae4eed55b91605635cf52a08e1bcefcd
Brar Piening <brar@gmx.de>
2019-12-29 00:15:52 +0100

    Create managed library and install it into the test environment

--- src/pl/plclr/plclr_runtime_host.c
+++ src/pl/plclr/plclr_runtime_host.c
@@ -1,4 +1,6 @@
 #include "postgres.h"
+#include "miscadmin.h"
+#include "port.h"
 #include "plclr_runtime_host.h"
 #include "nethost.h"
 
@@ -8,38 +10,84 @@
 #include "hostfxr.h"
 
 #include <dlfcn.h>
+#include <string.h>
 
 /* Forward declarations */
 void *get_export(void *, const char *);
+load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char* assembly);
 
-// Globals to hold hostfxr exports
-hostfxr_initialize_for_runtime_config_fn init_fptr;
-hostfxr_get_runtime_delegate_fn get_delegate_fptr;
-hostfxr_close_fn close_fptr;
+struct lib_args
+{
+    const char* message;
+    int number;
+};
+
+
+/* Globals to hold hostfxr exports */
+hostfxr_initialize_for_runtime_config_fn hostfxr_initialize;
+hostfxr_get_runtime_delegate_fn hostfxr_get_runtime_delegate;
+hostfxr_close_fn hostfxr_close;
 
 
 void
 plclr_runtime_host_init(void)
 {
-	size_t buffer_size;
-    char_t *buffer;
+    char path[MAXPGPATH];
+	size_t buffer_size = MAXPGPATH;
     int rc;
     void *lib;
+    load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer = NULL;
+    component_entry_point_fn hello = NULL;
+    const char* dotnet_type = "PlClrManaged.Lib, PlClrManaged";
+    const char* dotnet_type_method = "Hello";
 
-	get_hostfxr_path(NULL, &buffer_size, NULL);
-	buffer = palloc(buffer_size);
-	rc = get_hostfxr_path(buffer, &buffer_size, NULL);
+	rc = get_hostfxr_path(path, &buffer_size, NULL);
 	if (rc != 0)
-		elog(ERROR, "PL/CLR: Failed to get hostfxr path");
+		elog(ERROR, "Failed to get hostfxr path");
 
 	// Load hostfxr and get desired exports
-    lib = dlopen(buffer, RTLD_LAZY | RTLD_LOCAL);
+    lib = dlopen(path, RTLD_LAZY | RTLD_LOCAL);
     if (lib == NULL)
-        elog(ERROR, "PL/CLR failed to load %s: %s", buffer, dlerror());
+        elog(ERROR, "Failed to load %s: %s", path, dlerror());
 
-	init_fptr = (hostfxr_initialize_for_runtime_config_fn)get_export(lib, "hostfxr_initialize_for_runtime_config");
-	get_delegate_fptr = (hostfxr_get_runtime_delegate_fn)get_export(lib, "hostfxr_get_runtime_delegate");
-	close_fptr = (hostfxr_close_fn)get_export(lib, "hostfxr_close");
+	hostfxr_initialize = (hostfxr_initialize_for_runtime_config_fn)get_export(lib, "hostfxr_initialize_for_runtime_config");
+	hostfxr_get_runtime_delegate = (hostfxr_get_runtime_delegate_fn)get_export(lib, "hostfxr_get_runtime_delegate");
+	hostfxr_close = (hostfxr_close_fn)get_export(lib, "hostfxr_close");
+
+    path[0] = '\0';
+    get_lib_path(my_exec_path, path);
+    strcat(path, "/PlClrManaged.runtimeconfig.json");
+
+    load_assembly_and_get_function_pointer = get_dotnet_load_assembly(path);
+    if (load_assembly_and_get_function_pointer == NULL)
+        elog(ERROR, "Function get_dotnet_load_assembly(\"%s\") failed", path);
+
+    path[0] = '\0';
+    get_lib_path(my_exec_path, path);
+    strcat(path, "/PlClrManaged.dll");
+
+    
+    rc = load_assembly_and_get_function_pointer(
+        path,
+        dotnet_type,
+        dotnet_type_method,
+        NULL /*delegate_type_name*/,
+        NULL,
+        (void**)&hello);
+
+    if (rc == 0 || hello != NULL)
+        elog(ERROR, "Function load_assembly_and_get_function_pointer() failed: %s", path);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        struct lib_args args =
+        {
+            "from host!",
+            i
+        };
+
+        hello(&args, sizeof(args));
+    }
 }
 
 void *get_export(void *h, const char *name)
@@ -51,3 +99,29 @@ void *get_export(void *h, const char *name)
     return f;
 }
 
+load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t *config_path)
+{
+    // Load .NET Core
+    void *load_assembly_and_get_function_pointer = NULL;
+    hostfxr_handle cxt = NULL;
+    int rc;
+
+    rc = hostfxr_initialize(config_path, NULL, &cxt);
+    if (rc != 0 || cxt == NULL)
+    {
+        hostfxr_close(cxt);
+        elog(ERROR, "Init failed: %x", rc);
+    }
+
+    // Get the load assembly function pointer
+    rc = hostfxr_get_runtime_delegate(
+        cxt,
+        hdt_load_assembly_and_get_function_pointer,
+        &load_assembly_and_get_function_pointer);
+
+    if (rc != 0 || load_assembly_and_get_function_pointer == NULL)
+        elog(ERROR, "Get delegate failed: %x", rc);
+
+    hostfxr_close(cxt);
+    return (load_assembly_and_get_function_pointer_fn)load_assembly_and_get_function_pointer;
+}
