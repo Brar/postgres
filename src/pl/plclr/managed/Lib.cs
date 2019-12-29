1c4e3289a27d13cf334720b51958edafd7f722df
Brar Piening <brar@gmx.de>
2019-12-29 22:20:30 +0100

    Initial work on actually compiling something

--- src/pl/plclr/managed/Lib.cs
+++ src/pl/plclr/managed/Lib.cs
@@ -7,18 +7,62 @@ namespace PlClrManaged
     {
         private static int s_CallCount = 1;
 
+        private static string MarshalToString(IntPtr arg, string argName)
+        {
+            if (arg == IntPtr.Zero)
+            {
+                throw new ArgumentNullException(argName);
+            }
+
+#if PLATFORM_WINDOWS
+            string? result = Marshal.PtrToStringUni(arg);
+#else
+            string? result = Marshal.PtrToStringUTF8(arg);
+#endif
+
+            if (result == null)
+            {
+                throw new ArgumentNullException(argName);
+            }
+            return result;
+        }
+
         [StructLayout(LayoutKind.Sequential)]
         public struct LibArgs
         {
             public IntPtr Message;
             public int Number;
         }
-        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FunctionCompileInfo
+        {
+            public uint FunctionOid;
+            public IntPtr FunctionName;
+            public IntPtr FunctionBody;
+        }
+
+        public static int CompileAndExecute(IntPtr arg, int argLength)
+        {
+            if (argLength < Marshal.SizeOf(typeof(FunctionCompileInfo)))
+            {
+                return -1;
+            }
+
+            var compileInfo = Marshal.PtrToStructure<FunctionCompileInfo>(arg);
+            var functionName = MarshalToString(compileInfo.FunctionName, nameof(compileInfo.FunctionName));
+            var functionBody = MarshalToString(compileInfo.FunctionBody, nameof(compileInfo.FunctionBody));
+
+            Console.WriteLine($"Compiling function {functionName} (Oid: {compileInfo.FunctionOid}) ...");
+            Console.WriteLine($"Body: \"{functionBody}\"");
+            Console.WriteLine($"done.");
+            return 0;
+        }
+
         public static int Hello(IntPtr arg, int argLength)
         {
-            if (argLength < System.Runtime.InteropServices.Marshal.SizeOf(typeof(LibArgs)))
+            if (argLength < Marshal.SizeOf(typeof(LibArgs)))
             {
-                return 1;
+                return -1;
             }
 
             var libArgs = Marshal.PtrToStructure<LibArgs>(arg);
