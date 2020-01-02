53a2059a5bf43b6ec0466dcc2761140ac5a96a2b
Brar Piening <brar@gmx.de>
2020-01-02 01:14:52 +0100

    Add UpdateClrConfig tool
    UpdateClrConfig is a hacky tool to detect the system compatible runtime
    identifier that is used in  the path to nethost.(so|h)
    Also add the runtime identifier detection and fallback by that tool.

---
+++ src/tools/UpdateClrConfig/RuntimeIdentifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UpdateClrConfig
+{
+    internal sealed class RuntimeIdentifier
+    {
+        public RuntimeIdentifier(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public RuntimeIdentifier[]? CompatibleIdentifiers { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: [{(CompatibleIdentifiers == null ? "null" : string.Join(", ", CompatibleIdentifiers.Select(e => e.ToString())))}]";
+        }
+    }
+}
