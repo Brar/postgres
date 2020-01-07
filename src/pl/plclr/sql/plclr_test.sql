a265d67f8f76e86eccfbd35582c8bc2625e0e622
Brar Piening <brar@gmx.de>
2020-01-07 22:44:27 +0100

    Initial passing of arguments

--- src/pl/plclr/sql/plclr_test.sql
+++ src/pl/plclr/sql/plclr_test.sql
@@ -2,6 +2,11 @@
 CREATE EXTENSION IF NOT EXISTS plclr;
 
 -- simple function to get the module loaded
-CREATE FUNCTION plclr_regress_simple() RETURNS int AS '/* 😇 */return 1;' LANGUAGE plclr;
+CREATE FUNCTION plclr_regress_simple() RETURNS void AS '/* 😇 */' LANGUAGE plclr;
 
 select plclr_regress_simple();
+
+-- simple add function
+CREATE FUNCTION plclr_add(int, int) RETURNS int AS 'return arg1 + arg2;' LANGUAGE plclr;
+
+select plclr_add(21, 21);
