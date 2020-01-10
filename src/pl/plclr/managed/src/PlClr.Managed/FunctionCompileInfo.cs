7381c7c9b0f3f38f8e3e8fe25d21351f7e3fdd75
Brar Piening <brar@gmx.de>
2020-01-10 22:47:06 +0100

    Add usage of return values

--- src/pl/plclr/managed/src/PlClr.Managed/FunctionCompileInfo.cs
+++ src/pl/plclr/managed/src/PlClr.Managed/FunctionCompileInfo.cs
@@ -8,22 +8,26 @@ namespace PlClr
         public uint FunctionOid { get; }
         public string FunctionName { get; }
         public string FunctionBody { get; }
+        public uint ReturnValueType { get; }
+        public bool ReturnsSet { get; }
         public int NumberOfArguments { get; }
         public ImmutableArray<uint> ArgumentOids { get; }
         public ImmutableArray<string>? ArgumentNames { get; }
         public ImmutableArray<byte>? ArgumentModes { get; }
 
-        public FunctionCompileInfo(uint functionOid, string functionName, string functionBody) : this(functionOid,
-            functionName, functionBody, 0, Array.Empty<uint>(), null, Array.Empty<byte>())
+        public FunctionCompileInfo(uint functionOid, string functionName, string functionBody, uint returnValueType, bool returnsSet) : this(functionOid,
+            functionName, functionBody, returnValueType, returnsSet, 0, Array.Empty<uint>(), null, Array.Empty<byte>())
         {
         }
 
-        public FunctionCompileInfo(uint functionOid, string functionName, string functionBody, int numberOfArguments,
+        public FunctionCompileInfo(uint functionOid, string functionName, string functionBody, uint returnValueType, bool returnsSet, int numberOfArguments,
             uint[] argumentOids, string[]? argumentNames, byte[]? argumentModes)
         {
             FunctionOid = functionOid;
             FunctionName = functionName;
             FunctionBody = functionBody;
+            ReturnValueType = returnValueType;
+            ReturnsSet = returnsSet;
             NumberOfArguments = numberOfArguments;
             ArgumentOids = argumentOids.ToImmutableArray();
             ArgumentNames = argumentNames?.ToImmutableArray();
