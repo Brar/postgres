3d398d6188065ae803e3c94734855a54b2c7fa8e
Brar Piening <brar@gmx.de>
2020-01-27 21:24:21 +0100

    Fix managed tests

--- src/pl/plclr/managed/test/PlClr.Managed.Tests/FunctionCall.cs
+++ src/pl/plclr/managed/test/PlClr.Managed.Tests/FunctionCall.cs
@@ -1,7 +1,26 @@
 
+using System;
+
 namespace PlClr.Managed.Tests
 {
 	public class FunctionCall
 	{
-	}
+        private readonly PAllocDelegate _pAlloc;
+
+        public FunctionCall(PAllocDelegate pAllocDelegate)
+        {
+            _pAlloc = pAllocDelegate;
+        }
+
+        IntPtr DeToastDatum(IntPtr ptr) => ptr;
+
+        IntPtr GetAttributeByNum(IntPtr argPtr, short attNo, out bool isNull)
+        {
+            isNull = true;
+            return IntPtr.Zero;
+        }
+
+        public IntPtr GetDeToastDatumFunctionPointer() => System.Runtime.InteropServices.Marshal.GetFunctionPointerForDelegate<ReferenceTypeConversionDelegate>(DeToastDatum);
+        public IntPtr GetGetAttributeByNumFunctionPtr() => System.Runtime.InteropServices.Marshal.GetFunctionPointerForDelegate<GetAttributeByNumDelegate>(GetAttributeByNum);
+    }
 }
---
+++ src/pl/plclr/managed/test/PlClr.Managed.Tests/FunctionCompileResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PlClr.Managed.Tests
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FunctionCompileResult
+    {
+        public IntPtr ExecuteDelegatePtr;
+    }
+}
